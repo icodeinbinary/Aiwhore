2e32e520c66514c3886846dd269b1ac4e68bcabf
sourav singh <122343260+icodeinbinary@users.noreply.github.com>
2025-03-18 23:34:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
 					type="text"
 					className="bg-transparent focus:outline-none p-4 w-full placeholder:text-neutral-600 dark:placeholder:text-neutral-400"
 					required
-					placeholder="Ask me anything"
+					placeholder="Speak to me i'm listening"
 					value={input}
 					onChange={(e) => setInput(e.target.value)}
 					ref={inputRef}
@@ -171,14 +171,10 @@ export default function Home() {
 				{messages.length === 0 && (
 					<>
 						<p>
-							A fast, open-source voice assistant powered by{" "}
-							<A href="https://groq.com">Groq</A>,{" "}
-							<A href="https://x.ai">xAI</A>,{" "}
-							<A href="https://elevenlabs.io">ElevenLabs</A>,{" "}
-							<A href="https://www.vad.ricky0123.com/">VAD</A>,
-							and <A href="https://vercel.com">Vercel</A>.{" "}
+							Made with ❤️ by Sourav{" "}
+							
 							<A
-								href="https://github.com/ai-ng/swift"
+								href="https://t.me/icodeinbinary"
 								target="_blank"
 							>
 								Learn more
